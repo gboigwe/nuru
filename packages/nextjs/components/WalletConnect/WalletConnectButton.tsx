deeffd438546395cc68f40c67eacd53ce5d56409
DeborahOlaboye <deboraholaboye@gmail.com>
2025-12-01 16:17:26 +0100

    Added the useAutoConnect  hook for better wallet connection management

--- packages/nextjs/components/WalletConnect/WalletConnectButton.tsx
+++ packages/nextjs/components/WalletConnect/WalletConnectButton.tsx
@@ -26,7 +26,8 @@ export const WalletConnectButton = () => {
     setShowDropdown(false);
   };
 
-  const formatAddress = (addr: string) => {
+  const formatAddress = (addr: string | undefined) => {
+    if (!addr) return '';
     return `${addr.slice(0, 6)}...${addr.slice(-4)}`;
   };
 
