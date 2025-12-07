9366fdc3a0cd9016d0a910c4cf9b560527f602ab
DeborahOlaboye <deboraholaboye@gmail.com>
2025-12-07 08:49:03 +0100

    feat: Add onramp components export index
    - Export all onramp components from single entry point
    - Include FundWalletCard, MobileMoneyOnramp, FiatSelector, OnrampModal
    - Export FiatCurrency type for TypeScript support
    - Simplify imports across the application
    
    Part of #85 - Developer experience improvement

---
+++ packages/nextjs/components/onramp/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Onramp Components
+ *
+ * Export all onramp-related components for easy importing
+ */
+
+export { FundWalletCard } from './FundWalletCard';
+export { MobileMoneyOnramp } from './MobileMoneyOnramp';
+export { FiatSelector } from './FiatSelector';
+export { OnrampModal } from './OnrampModal';
+
+export type { FiatCurrency } from './FiatSelector';
