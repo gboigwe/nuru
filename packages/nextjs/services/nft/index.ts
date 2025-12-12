16e2603feb68bd2d360913a99120383051c42498
Gbolahan Akande <geakande@gmail.com>
2025-12-12 16:30:50 +0100

    Add receipt minting service

--- packages/nextjs/services/nft/index.ts
+++ packages/nextjs/services/nft/index.ts
@@ -6,3 +6,4 @@ export * from "./ReceiptMetadataService";
 export * from "./ReceiptImageGenerator";
 export * from "./ReceiptPDFExport";
 export * from "./ReceiptVerificationService";
+export * from "./ReceiptMintingService";
