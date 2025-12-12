21a64329db5676e8b31aac8e9dd41de58588812f
Gbolahan Akande <geakande@gmail.com>
2025-12-12 21:52:27 +0100

    Add NLP services index and documentation

---
+++ packages/nextjs/services/nlp/index.ts
@@ -0,0 +1,10 @@
+/**
+ * NLP Services Export
+ *
+ * Advanced Natural Language Processing for Voice Commands
+ */
+
+export * from "./OpenAIService";
+export * from "./LanguageDetector";
+export * from "./ContextManager";
+export * from "./EnhancedVoiceCommandProcessor";
